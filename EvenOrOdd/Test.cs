c1ce9babd68e4d7c72f59b77027e7d830e9d56c7
Greg Doud <gpdoud@gmail.com>
2021-05-19 11:27:24 -0400

    Add project files.

---
+++ EvenOrOdd/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvenOrOdd {
+    class Test {
+
+        public static void Run() {
+            var ints = new int[] { 1, 2, 3, 4 };
+            {
+                //var index = 0;
+                //while(index < 4) {
+                //    Console.WriteLine($"Index {index} of ints is {ints[index]}");
+                //    index++;
+                //}
+            }
+
+            for(var index = 0; index < 4; index++) {
+                Console.WriteLine($"Index {index} of ints is {ints[index]}");
+            }
+        }
+    }
+}
